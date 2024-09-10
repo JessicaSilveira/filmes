781389474c5aeab57e823595c86414a8caa7607e
Jess <j.silva07@hotmail.com>
2024-09-10 11:48:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Deadpool & Wolverine",
+        descricao: "Em 'Deadpool & Wolverine', a Marvel nos presenteia com uma explosão de ação e humor negro. O mercenário tagarela mais famoso dos quadrinhos se junta ao mutante com garras de adamantium em uma aventura explosiva, repleta de piadas irreverentes e sequências de luta épicas. A dinâmica entre os dois personagens carismáticos é o ponto alto do filme, que promete divertir tanto os fãs de quadrinhos quanto os espectadores casuais.",
+        link: "https://www.disney.com.br/filmes/deadpool-&-wolverine",
+        tags: "deadpool"
+    },
+    {
+        titulo: "Barbie",
+        descricao: "Barbie, o filme, é uma aventura colorida e divertida que leva a icônica boneca de plástico para uma jornada de autodescoberta. Em Barbieland, um mundo perfeito onde tudo é cor-de-rosa, Barbie começa a questionar sua existência e o significado da vida. A comédia, repleta de referências à cultura pop, se mistura com temas profundos sobre identidade e empoderamento feminino, convidando o público a refletir sobre seus próprios valores..",
+        link: "https://www.imdb.com/title/tt1517268/",
+        tags: "boneca"
+    },
+    {
+        titulo: "Homem-Aranha no Aranhaverso",
+        descricao: "Homem-Aranha no Aranhaverso é uma revolução na animação, que explora um multiverso vibrante e colorido, repleto de diferentes versões do Homem-Aranha. Com uma animação única e inovadora, o filme apresenta um protagonista negro, Miles Morales, e desafia os estereótipos do gênero de super-heróis. Uma obra-prima da animação que encanta tanto crianças quanto adultos.",
+        link: "https://www.adorocinema.com/filmes/filme-269975/",
+        tags: "homem aranha"
+    }
+];
